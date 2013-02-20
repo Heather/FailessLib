a2d1f4c5f3ae9566a28524420cdbd0dc33dd3a07
Heather Mason <heather@cynede.net>
2013-02-20 16:28:56 +0400

    Failess lib init

---
+++ CSSCore.fs
@@ -0,0 +1,9 @@
+﻿[<AutoOpen>]
+module Failess.CSSCore
+open System
+open System.ComponentModel
+
+let inline (+++) a b = sprintf "%s%s" a b
+let inline (++) a b = sprintf "%s %s" a b
+
+let inline s str = str.ToString()
